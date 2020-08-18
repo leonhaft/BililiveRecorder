d4ec9792a79972821a3761cbf69bab0dfd141859
leonhaft <leonhaft@outlook.com>
2020-08-18 20:16:42 +0800

    将WPF项目改为.net Core

---
+++ BililiveRecorder.WPF/BuildInfo.WPF.cs
@@ -0,0 +1,10 @@
+namespace BililiveRecorder.WPF
+{
+    internal class BuildInfo
+    {
+        internal const bool Appveyor = false;
+        internal const string Version = @"1.1.18";
+        internal const string HeadSha1 = @"2dfb3b7de3f71ecd10173d7ffedcfe61694d6c09";
+        internal const string HeadShaShort = @"2dfb3b7d";
+    }
+}
